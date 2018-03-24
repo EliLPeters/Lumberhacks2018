b3c80eb78447432a53b14ea09477974023518ac9
JMackin <Johnluthermackin@gmail.com>
2018-03-23 21:21:15 -0700

    added movement

--- NetbeansProject/game.h
+++ NetbeansProject/game.h
@@ -22,6 +22,7 @@ public:
     game()
     {
         void launch();
+        void play();
     }
     
     void hello()
@@ -31,6 +32,68 @@ public:
     
     void launch()
     {
+        initscr();
+        clear();
+        noecho();
+        cbreak();
+        keypad(stdscr, TRUE);
+        curs_set(0);
+        printw("Welcome to the game.");
+        getch();
+        clear();
+    }
+    
+    void play()
+    {
+        char ch;
+        int row = 10;
+        int col = 10;
+        char guy = '@';
+        mvaddch(row,col,guy);
+        
+        while(true)
+        {
+            ch = getch();
+            
+            if(ch == 'q')
+            {
+                clear();
+                break;
+            }
+            else if(ch == 'i')
+            {
+                clear();
+                row--;
+                mvaddch(row,col,guy);
+            }
+            else if(ch == 'k')
+            {
+                clear();
+                row++;
+                mvaddch(row,col,guy);
+            }
+            else if(ch == 'l')
+            {
+                clear();
+                col++;
+                mvaddch(row,col,guy);
+            }
+            else if(ch == 'j')
+            {
+                clear();
+                col--;
+                mvaddch(row,col,guy);
+            }
+            else
+            {
+                clear();
+                printw("yeah");
+            }
+          
+            refresh();
+        }
+        
+        return;
     }
     
     ~game()
