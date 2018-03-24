d4ce528264f4237c0a7dc211c89a0c80062fb1af
Eli Peters <elp5@humboldt.edu>
2018-03-23 21:32:18 -0700

    added map.h

---
+++ NetbeansProject/map.h
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   map.h
+ * Author: eli
+ *
+ * Created on March 23, 2018, 9:30 PM
+ */
+
+#ifndef MAP_H
+#define MAP_H
+
+class map
+{
+    map()
+    {
+        
+    }
+};
+
+#endif /* MAP_H */
+
